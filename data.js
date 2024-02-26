45733ebe65cefacda6258979a4fc5fb0382b7c22
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2024-02-26 10:19:53 +0100

    Separated songs list from general script

---
+++ data.js
@@ -0,0 +1,5 @@
+exampleSheets = {
+    "Ay linda amiga!": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/ay-linda-amiga.musicxml",
+    "L'Amo de Son Carabassa": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/lamo-de-son-carabassa.musicxml",
+    "Happy X-Mas War Is Over": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/happy-x-mas-war-is-over.musicxml",
+}
