a115b513c7f5fa4ffd65ad0298f301c50345e39a
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2024-02-26 11:33:26 +0100

    Added assets

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
 exampleSheets = {
+    "What Was I Made For": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/what-was-i-made-for.musicxml",
+    "Bach: Adagio BWV 974": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/adagio-bwv-974.musicxml",
     "Ay linda amiga!": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/ay-linda-amiga.musicxml",
     "L'Amo de Son Carabassa": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/lamo-de-son-carabassa.musicxml",
     "Happy X-Mas War Is Over": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/happy-x-mas-war-is-over.musicxml",
