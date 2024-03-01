e1835d5863a232b830275bff7b71257fead00dfc
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2024-03-01 14:22:42 +0100

    Added: The Moldau

--- data.js
+++ data.js
@@ -4,4 +4,5 @@ exampleSheets = {
     "Ay linda amiga!": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/ay-linda-amiga.musicxml",
     "L'Amo de Son Carabassa": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/lamo-de-son-carabassa.musicxml",
     "Happy X-Mas War Is Over": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/happy-x-mas-war-is-over.musicxml",
+    "The Moldau": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/the-moldau.musicxml",
 }
